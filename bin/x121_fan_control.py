eef8788f54683efcb48ca5b5e823ae01ac9d91cd
oz123 <nahumoz@gmail.com>
2012-09-02 23:17:31 +0200

    improve structre, do actual contorolling.

--- bin/x121_fan_control.py
+++ bin/x121_fan_control.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
-#
 """ 
    
    x121_fan_control.py 
-
 
 #  Copyright 2012 Oz N <nahumoz__AT_NONONO_SPAMHERE g m a i l dot com>
 #  
@@ -30,26 +26,75 @@
 import re
 import ConfigParser,sys
 import os
-#from optparse import OptionParser
 import argparse
+import time
+import subprocess as sp
 
 # max allowed temp: level 
-
-
-#LEVELS = (0,  1,  2,  5,  6,  7)
-#LIMITS = (0, 53, 63, 67, 73, 100)
+MAXTEMP_LEVEL = {53:0, 60:1, 64:2, 70:5, 75:7}
 
 BASE_POLL_TIME_INTERVAL = 10
 TEMP_SENSOR = "/proc/acpi/ibm/thermal"
 FAN_INFO = "/proc/acpi/ibm/fan"
 
+# todo:
+# 1. add pid file under /var/run
+# 2. add option to damonize
+# 3. add logging (python logging module?)
+# 4. document properly
+
+def best_fan_level(clevel,ctemp):
+    """
+    in case fan level is auto, we need to find the right level to 
+    start with ...
+    """
+    nearest_temp=min(MAXTEMP_LEVEL.keys(), key=lambda k: abs(k-ctemp))
+    # this is the same as doing:
+    # temp_d={}
+    # for k in MAXTEMP_LEVEL.keys():
+    #     temp_d[abs(k-ctemp)] = k
+    # print temp_d[min(temp_d.key())]
+    #if clevel < MAXTEMP_LEVEL[mintemp]:
+    #    print "current level is", str(clevel), "we should be on ",str(MAXTEMP_LEVEL[mintemp])
+    #print "T: %d L: %d  Tnear: %d Levelnear: %d" % (ctemp, 
+    #clevel, nearest_temp, MAXTEMP_LEVEL[nearest_temp])
+    
+    if ctemp >= nearest_temp and clevel < MAXTEMP_LEVEL[nearest_temp]:
+        print "T: %d L: %d  Tnear: %d Levelnear: %d, fan increased" % (ctemp, 
+    clevel, nearest_temp, MAXTEMP_LEVEL[nearest_temp])
+    if ctemp < nearest_temp and clevel > MAXTEMP_LEVEL[nearest_temp]:
+        print "T: %d L: %d  Tnear: %d Levelnear: %d, fan decreased" % (ctemp, 
+    clevel, nearest_temp, MAXTEMP_LEVEL[nearest_temp])
+       
+    elif ctemp < nearest_temp and clevel == MAXTEMP_LEVEL[nearest_temp]:
+        print "T: %d L: %d  Tnear: %d Levelnear: %d, fan safe" % (ctemp, 
+    clevel, nearest_temp, MAXTEMP_LEVEL[nearest_temp])
+    
+    elif ctemp > nearest_temp and clevel == MAXTEMP_LEVEL[nearest_temp]:
+        print "T: %d L: %d  Tnear: %d Levelnear: %d, fan safe*" % (ctemp, 
+    clevel, nearest_temp, MAXTEMP_LEVEL[nearest_temp])
+        
+    
+    #print "sent ", MAXTEMP_LEVEL[nearest_temp]
+    return MAXTEMP_LEVEL[nearest_temp]
+
+            
+def set_level(new_level):
+    """
+    set fan to new level
+    """
+    cmd = "echo \"level "+str(new_level)+"\" >"+FAN_INFO
+    #print cmd
+    setcmd = sp.Popen(cmd, shell=True, stderr=sp.PIPE, stdout=sp.PIPE)
+    err, out = setcmd.communicate()
+    
 def poll():
     with open(TEMP_SENSOR) as ts:
         ts = ts.next()
-        print ts.strip('\n')
+        #print ts.strip('\n')
         ts = re.search('\d+', ts)
         cputemp = ts.group()
-        print "cputemp ", cputemp
+        #print "cputemp ", cputemp
     with open(FAN_INFO) as faninfo:
         faninfo = faninfo.readlines()
         faninfo = faninfo[2]
@@ -58,42 +103,48 @@ def poll():
         else:
             faninfo = re.search('\d+', faninfo)
             fan_level = faninfo.group()
-            #fan_level = faninfo.split(':')
-        print  "fan_level ", fan_level
+        #print  "fan_level ", fan_level
     return fan_level, cputemp
 
-
-
+def loop_sleep(args):
+    """
+    the polling loop
+    """
+    fanlevel, cpu_temp = poll()
+    if fanlevel == 'auto':
+        fanlevel = 0
+                
+    continue_polling = True
+    while continue_polling:
+        newlevel=best_fan_level(int(fanlevel), int(cpu_temp))
+        set_level(newlevel)
+        
+        fanlevel, cpu_temp = poll()
+        if fanlevel == 'auto':
+            fanlevel = 0
+        time.sleep(5)
+            
 
 def main():
+    if not os.getuid() == 0:
+        print "You must be root to change fan speed."
+        sys.exit(2)
+        
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description='Control the Fan of Lenovo \
     thinkpad x121e')
     parser.add_argument('--poll',help='Poll the tempratures, do nothing \
     real', action="store_true")
-    args = parser.parse_args()
+    
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+    args=parser.parse_args()
     
     if args.poll:
         print "polling turned on"
-        fanlevel, cpu_temp = poll()
-    # do the actual control
-    if fanlevel == 'auto':
-        fanlevel = 0
-    print "fanlevel ", fanlevel
-    # dictionaries suck!
-    MAXTEMP_LEVEL = {53:0, 56:1, 58:2, 70:5, 75:7}
-    for k,v in MAXTEMP_LEVEL.iteritems():
-        print "max temp allowed for the level ", k, "is ",v
-        if int(fanlevel) >= v and int(cpu_temp) < k:
-            print "fan is overworking will reduce one level"
-            print "call to some_reduce_function"
-            break
-        if int(fanlevel) <= v and int(cpu_temp) > k:
-            print "fan is under working will increase one level"
-            print "call to some_increase_function"
-            break
-            
-
+        loop_sleep(args)
+        
 if __name__ == '__main__':
     main()
 
