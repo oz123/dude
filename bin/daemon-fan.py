eab9973858596e1702abefea0f753fbdd5e3efaf
oz123 <nahumoz@gmail.com>
2012-12-16 15:38:52 +0100

    - add check_proc, better error message - add doc strings to functions and methods - add new function perm_module_load

--- bin/daemon-fan.py
+++ bin/daemon-fan.py
@@ -48,14 +48,20 @@ TEMP_SENSOR = "/proc/acpi/ibm/thermal"
 FAN_INFO = "/proc/acpi/ibm/fan"
 
 class FanControlDaemon(Daemon):
-    
+    """
+    define the FanControl Daemon and methods to:
+    - manipulate fan speed.
+    - determine temperatures
+    - determine the fan speed
+    - determine appropriate fan speed at right temp
+    """
     def best_fan_level(self,c_fan_level,ctemp):
         """
-        in case fan level is auto, we need to find the right level to 
+        Determine the best fan speed based on temperature.
+        In case fan level is auto, we need to find the right level to 
         start with ...
         """
         nearest_temp = min(MAXTEMP_LEVEL.keys(), key=lambda k: abs(k-ctemp))
-        #print "nt " ,  nearest_temp, "ctemp", ctemp
         allowed_fan_level = MAXTEMP_LEVEL[nearest_temp]
         if ctemp >= nearest_temp and c_fan_level <= allowed_fan_level:
             print "T: %d L: %d  Tnear: %d Levelnear: %d, fan increased" % (ctemp, 
@@ -77,12 +83,13 @@ class FanControlDaemon(Daemon):
         
         
     def poll(self):
+        """
+        get the the tempratures
+        """
         with open(TEMP_SENSOR) as ts:
             ts = ts.next()
-            #print ts.strip('\n')
             ts = re.search('\d+', ts)
             cputemp = ts.group()
-            #print "cputemp ", cputemp
         with open(FAN_INFO) as faninfo:
             faninfo = faninfo.readlines()
             faninfo = faninfo[2]
@@ -91,7 +98,6 @@ class FanControlDaemon(Daemon):
             else:
                 faninfo = re.search('\d+', faninfo)
                 fan_level = faninfo.group()
-            #print  "fan_level ", fan_level
         return fan_level, cputemp    
     
     def set_level(self,new_level):
@@ -103,19 +109,20 @@ class FanControlDaemon(Daemon):
         err, out = setcmd.communicate()
         
     def run(self):
+        """
+        execute the main fan control loop
+        """
         while True:
             fanlevel, cpu_temp = self.poll()
             if fanlevel == 'auto':
                 fanlevel = 0
-                    
             newlevel = self.best_fan_level(int(fanlevel), int(cpu_temp))
             self.set_level(newlevel)
             time.sleep(5)
     
-    
-    def start(self):
+    def check_proc(self):
         """
-        Start in forground
+        check for pid file, create pid file
         """
         # Check for a pidfile to see if the daemon already runs
         try:
@@ -124,13 +131,18 @@ class FanControlDaemon(Daemon):
             pf.close()
         except IOError:
             pid = None
-    
         if pid:
-            message = "pidfile %s already exist. Daemon already running?\n"
-            sys.stderr.write(message % self.pidfile)
+            message = "pidfile %s already exists. Daemon already running?\n"
+            message += "check if process %d still exists\n"
+            sys.stderr.write(message % (self.pidfile, pid))
             sys.exit(1)
         
-        
+    def start(self):
+        """
+        Start in forground
+        """
+        # Check for a pidfile to see if the daemon already runs
+        self.check_proc()
         pid = str(os.getpid())
         print "[pid: %s]" % pid   
         open(self.pidfile,'w+').write("%s\n" % pid)
@@ -141,19 +153,10 @@ class FanControlDaemon(Daemon):
         Start the daemon
         """
         # Check for a pidfile to see if the daemon already runs
-        try:
-            pf = open(self.pidfile,'r')
-            pid = int(pf.read().strip())
-            pf.close()
-        except IOError:
-            pid = None
-    
-        if pid:
-            message = "pidfile %s already exist. Daemon already running?\n"
-            sys.stderr.write(message % self.pidfile)
-            sys.exit(1)
-        
+        self.check_proc()
         # Start the daemon
+        pid = str(os.getpid())
+        open(self.pidfile,'w+').write("%s\n" % pid)
         self.daemonize()
         self.run()
 
@@ -164,19 +167,18 @@ class FanControlDaemon(Daemon):
         self.stop()
         self.startd()
 
-
-def load_module(module_name):
+def load_module(module_name, opts=''):
     """
     wrap around modprobe -i module_name
     """
-    cmd = "modprobe -i %s" % module_name
+    cmd = "modprobe -i %s %s" % module_name, opts
     modprobe = sp.Popen(cmd, shell=True)
     stat = modprobe.poll()
     if stat != 0:
         print "could not load module %s" % module_name
         sys.exit(2)
 
-def module_cfg(mname,syscfgfile, param)
+def chk_module_cfg(mname,syscfgfile, param):
     """
     check in /sys/module that the module was loaded with the correct
     parameters.
@@ -190,6 +192,15 @@ def module_cfg(mname,syscfgfile, param)
         return 1
     return 0
 
+def perm_module_load(module_name):
+    """
+    add module to /etc/modules so that moduels load 
+    at boot time.
+    Is this unique to Debian?
+    """
+    with open('/etc/modules', 'a') as modules:
+        modules.write(module_name+'\n')
+        
 def check_config():
     """
     this function checks that we can change the fan speed
@@ -198,31 +209,32 @@ def check_config():
     lsmod | grep thinkpad_acpi
     grep /etc/modprobe.d/thinkfan.conf
     """        
-    print "not yet implemented"
     # first that we have the modules loades
-    loaded_modules = [ m.name for m in km.loaded() ]
+    loaded_modules = [ m.name for m in km.loaded() ]  
     if u"thinkpad_acpi" not in loaded_modules:
-        load_module("thinkpad_acpi")
-        cfgval = module_cfg("thinkpad_acpi",
-        "/sys/module/thinkpad_acpi/parameters/fan_control", 'Y')
-        if cfgval != 0:
-            print "You should configure thinkpad_acpi with fan_control=1."
-            print """
+        load_module("thinkpad_acpi", "fan_control=1")
+    cfgval = chk_module_cfg("thinkpad_acpi",
+    "/sys/module/thinkpad_acpi/parameters/fan_control", 'Y')
+    if cfgval != 0:
+        print "You should configure thinkpad_acpi with fan_control=1."
+        print """
 edit /etc/modprobe.d/thinkfan.conf to contain: 
 options thinkpad_acpi fan_control=1 
 Would you like to fix it now [Y/n]?
 """
         ans = raw_input()
-                if ans.lower() == 'y':
-            #km.modprobe is broken
-            load_module("coretemp")
+        if ans.lower() == 'y':
+            conf = open("/etc/modprobe.d/thinkfan.conf", 'a')
+            conf.write('\noptions thinkpad_acpi fan_control=1\n')
+            conf.close()    
         else:
             print "Refusing to continue... this could harm your computer..."
             print "You should really load thinkpad_acpi to run this script" 
-            
-            
+            sys.exit(2)
+        perm_module_load("thinkpad_acpi")
     if u"coretemp" not in loaded_modules:
-        # module is not loaded!   
+        load_module("coretemp")
+        #module is not loaded!   
         #raise EnvironmentError("Module 'coretemp is not loaded'")
         ans = 'Y'
         ans = raw_input("Module 'coretemp is not loaded', would you ", 
@@ -230,29 +242,20 @@ Would you like to fix it now [Y/n]?
         if ans.lower() == 'y':
             #km.modprobe is broken
             load_module("coretemp")
+            perm_module_load("core_temp")
         else:
             print "Refusing to continue... this could harm your computer..."
             print "You should really load thinkpad_acpi to run this script" 
-            
-            
+            sys.exit(2)
         
-        
-        
-        sys.exit(2)
-    
     #cat /etc/modprobe.d/thinkfan.conf 
     #options thinkpad_acpi fan_control=1
-
-    
-
     #cfg = open("/sys/module/thinkpad_acpi/parameters/fan_control")   
     #if cfg.readline().strip() != "Y":
         #raise EnvironmentError("misconfigured")
 
-
-
 if __name__ == "__main__":
-    daemon = FanControlDaemon('/tmp/thinkpadfan.pid')
+    daemon = FanControlDaemon('/var/run/thinkpadfan.pid')
     if not os.getuid() == 0:
         print "You must be root to change fan speed."
         sys.exit(2)
@@ -262,7 +265,7 @@ if __name__ == "__main__":
     parser.add_argument('-p','--poll',help='Poll the tempratures, do nothing \
     real', action="store_true")
     parser.add_argument('-D','--NoDaemon', help='Do not fork, start in foreground', 
-    action="store_true")
+    action="store_true", default=False)
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -284,6 +287,8 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             daemon.set_level("auto")
             daemon.stop()
+    if action == 'start' and not args.NoDaemon:
+        daemon.startd()
     if action == "restart":
         daemon.restart()
     if action == "stop":
