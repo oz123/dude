ee4105fc94d143b8a1999088bf55c88b28637018
oz123 <nahumoz@gmail.com>
2012-12-22 10:30:07 +0100

    add self.name

--- bin/daemon.py
+++ bin/daemon.py
@@ -9,12 +9,12 @@ class Daemon:
     
     Usage: subclass the Daemon class and override the run() method
     """
-    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    def __init__(self, name, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
         self.pidfile = pidfile
-    
+        self.name = name
     def daemonize(self):
         """
         do the UNIX double-fork magic, see Stevens' "Advanced 
