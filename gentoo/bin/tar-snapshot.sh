953af3afd17bcfab316de806b9c1f6701f93c764
oz123 <oz.tiram@gmail.com>
2016-11-21 09:17:55 +0100

    Exclude home

--- gentoo/bin/tar-snapshot.sh
+++ gentoo/bin/tar-snapshot.sh
@@ -3,11 +3,14 @@
 # not the best in the world, but does the job of preserving my gentoo installation
 # mount your gentoo partition and back it up ...
 
-tar czf ../gentoo.backup-`date +%Y-%m-%d`.tar.gz --exclude=./run \
+DIROUT=$1
+
+tar czf $DIROUT/gentoo.backup-`date +%Y-%m-%d`.tar.gz --exclude=./run \
 	--exclude=./lost+found \
 	--exclude=./tmp  \
 	--exclude=./dev \
 	--exclude=./proc \
+	--exclude=./home \
 	--exclude=./usr/portage/distfiles \
 	--exclude=./usr/portage/packages \
 	--exclude=./var/lib/docker \
