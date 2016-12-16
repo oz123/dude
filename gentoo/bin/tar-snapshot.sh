d70d67a58b02246f5e5ffbf45b2bb664778c6fea
Oz N Tiram <oz.tiram@gmail.com>
2016-12-16 22:53:13 +0100

    Fix root directory

--- gentoo/bin/tar-snapshot.sh
+++ gentoo/bin/tar-snapshot.sh
@@ -3,7 +3,14 @@
 # not the best in the world, but does the job of preserving my gentoo installation
 # mount your gentoo partition and back it up ...
 
+if [ -z $2 ]; then
+	ROOT=$2
+else
+	ROOT="."
+fi
+
 DIROUT=$1
+
 
 tar czf $DIROUT/gentoo.backup-`date +%Y-%m-%d`.tar.gz --exclude=./run \
 	--exclude=./lost+found \
@@ -14,5 +21,5 @@ tar czf $DIROUT/gentoo.backup-`date +%Y-%m-%d`.tar.gz --exclude=./run \
 	--exclude=./usr/portage/distfiles \
 	--exclude=./usr/portage/packages \
 	--exclude=./var/lib/docker \
-	--exclude=./sys  .
+	--exclude=./sys ${ROOT}
 
