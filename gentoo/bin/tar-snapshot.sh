188f3d13fa8f2ceb98d69638ca7c03813343dfd3
oz123 <oz.tiram@gmail.com>
2017-07-01 07:42:33 +0200

    Improve excludes

--- gentoo/bin/tar-snapshot.sh
+++ gentoo/bin/tar-snapshot.sh
@@ -9,16 +9,20 @@ DIROUT=$1
 
 
 tar vczf $DIROUT/gentoo.backup-`date +%Y-%m-%d`.tar.gz \
-	--exclude=${ROOT}run \
-	--exclude=${ROOT}lost+found \
-	--exclude=${ROOT}tmp  \
-	--exclude=${ROOT}dev \
-	--exclude=${ROOT}proc \
-	--exclude=${ROOT}home \
-	--exclude=${ROOT}mnt \
-	--exclude=${ROOT}usr/portage/distfiles \
-	--exclude=${ROOT}usr/portage/packages \
-	--exclude=${ROOT}var/lib/docker \
-	--exclude=${ROOT}var/log \
-	--exclude=${ROOT}sys ${ROOT}
+	--exclude=${ROOT}run/media/* \
+	--exclude=${ROOT}root/* \
+	--exclude=${ROOT}run/* \
+	--exclude=${ROOT}lost+found/* \
+	--exclude=${ROOT}tmp/*  \
+	--exclude=${ROOT}dev/* \
+	--exclude=${ROOT}proc/* \
+	--exclude=${ROOT}home/* \
+	--exclude=${ROOT}mnt/* \
+	--exclude=${ROOT}usr/portage/distfiles/* \
+	--exclude=${ROOT}usr/portage/packages/* \
+	--exclude=${ROOT}var/tmp/portage/* \
+	--exclude=${ROOT}var/tmp/genkernel/* \
+	--exclude=${ROOT}var/lib/docker/* \
+	--exclude=${ROOT}var/log/* \
+	--exclude=${ROOT}sys/* ${ROOT}
 
