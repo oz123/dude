adb08d60840d79eb0bfc4953e188bd5c760c58ac
oz123 <oz.tiram@gmail.com>
2017-11-05 19:55:02 +0100

    Add option to download the jre or jdk

--- gentoo/bin/get_oracle_jdk_linux_x64.sh
+++ gentoo/bin/get_oracle_jdk_linux_x64.sh
@@ -5,17 +5,18 @@
 # usage: get_jdk.sh <jdk_version> <ext>
 # jdk_version: 8(default) or 9
 # ext: rpm or tar.gz
+# jre: jre or jdk
 
 # example usage: ./get_oracle_jdk_linux_x64.sh 8 tar.gz
 # origin: https://gist.github.com/n0ts/40dd9bd45578556f93e7
 jdk_version=${1:-8}
 ext=${2:-rpm}
-
+jre=${3:-jdk}
 readonly url="http://www.oracle.com"
 readonly jdk_download_url1="$url/technetwork/java/javase/downloads/index.html"
 readonly jdk_download_url2=$(
     curl -s $jdk_download_url1 | \
-    egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${jdk_version}-downloads-.+?\.html" | \
+    egrep -o "\/technetwork\/java/\javase\/downloads\/${jre}${jdk_version}-downloads-.+?\.html" | \
     head -1 | \
     cut -d '"' -f 1
 )
@@ -24,7 +25,7 @@ readonly jdk_download_url2=$(
 readonly jdk_download_url3="${url}${jdk_download_url2}"
 readonly jdk_download_url4=$(
     curl -s $jdk_download_url3 | \
-    egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/jdk-${jdk_version}.*(-|_)linux-(x64|x64_bin).$ext"
+    egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/${jre}-${jdk_version}.*(-|_)linux-(x64|x64_bin).$ext"
 )
 
 for dl_url in ${jdk_download_url4[@]}; do
